dcf6c3f688089db048cc97b1de9aef8bb1c47a76
Charles Flèche <charles.fleche@free.fr>
2020-07-11 13:23:29 -0400

    rclone to /srv and put config in /usr/local/etc

--- roles/gdrivesync/files/gdrive-sync.sh
+++ roles/gdrivesync/files/gdrive-sync.sh
@@ -1,4 +1,6 @@
-rclone --config "/home/gdrive/rclone.conf" --drive-shared-with-me sync remote: /home/gdrive/data/
+#!/usr/bin/bash
+
+rclone --config "/usr/local/etc/rclone/rclone.conf" --drive-shared-with-me sync remote: /srv/rclone/gdrive-sync
 
 # Launch backup
 /bin/bash /usr/bin/backup/backup.sh  # TODO: should be linked by a systemd timer
