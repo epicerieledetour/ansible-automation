3412221621f4d84d8004bc9f334a06268f170dbd
Charles Flèche <charles.fleche@free.fr>
2020-07-11 15:14:30 -0400

    Uncouple rclone / backup

--- roles/gdrivesync/files/gdrive-sync.sh
+++ roles/gdrivesync/files/gdrive-sync.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
 
 rclone --config "/usr/local/etc/rclone/rclone.conf" --drive-shared-with-me sync remote: /srv/rclone/gdrive-sync
-
-# Launch backup
-/bin/bash /usr/bin/backup/backup.sh  # TODO: should be linked by a systemd timer
